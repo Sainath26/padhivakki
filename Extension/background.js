c332bf328fd0ab5349d1d2ee855730214c6bfa34
Sainath26 <70280910+Sainath26@users.noreply.github.com>
2024-03-04 17:10:27 +0530

    gettting the selected text and URL

--- Extension/background.js
+++ Extension/background.js
@@ -1,5 +1,9 @@
 chrome.contextMenus.create({
-  id: "savethis",
-  title: "Save this",
+  id: "savethisinyourlocalfile",
+  title: "Save this in your local file",
   contexts: ["selection"],
+});
+
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  console.log(item.selectionText);
 });
